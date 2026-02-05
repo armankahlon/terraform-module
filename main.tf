ee2e9306dfca0d491e45c14fe318e24da8e8bb07
armankahlon <arman932010@gmail.com>
2026-02-05 15:09:36 -0800

    Adding descriptions

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
+terraform {
+  required_version = ">= 1.0"
+}
+
 output "name" {
+  description = "Applicant name"
   value = "Armandeep Kahlon"
 }
 
 output "position_applied_for" {
+  description = "Position being applied to"
   value = "Senior DevOps Engineer"
 }
